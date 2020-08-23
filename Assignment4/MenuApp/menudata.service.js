4a56edd6b98b823f6d51d816e78222263681be44
Fatimah-Tariq <68962809+Fatimah-Tariq@users.noreply.github.com>
2020-08-23 13:40:03 +0500

    Create menudata.service.js

---
+++ Assignment4/MenuApp/menudata.service.js
@@ -0,0 +1,35 @@
+(function(){
+
+    "use strict";
+    angular.module("MenuApp")
+           .service('MenuDataService', MenuDataService );
+          
+    MenuDataService.$inject = ['$http']
+    function MenuDataService($http){
+        var service = this;
+
+        service.getAllCategories = function(){
+            var service = this;
+  
+            service.response = $http({
+              method: "GET",
+              url: ("https://davids-restaurant.herokuapp.com/categories.json")
+            });
+        
+            return service.response;
+      }
+
+      service.getItemsForCategory = function(item){
+        var service = this;
+        console.log(item);
+        service.response = $http({
+          method: "GET",
+          url: ("https://davids-restaurant.herokuapp.com/menu_items.json?category=" + item.short_name)
+        });
+    
+        return service.response;
+      }
+        
+    }
+    
+})();
